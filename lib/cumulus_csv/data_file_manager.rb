8aa990a46fb23c63a6632c15b1f3ec0819947745
evizitei <ethan.vizitei@gmail.com>
2010-03-09 21:00:31 +0000

    add documentation

--- lib/cumulus_csv/data_file_manager.rb
+++ lib/cumulus_csv/data_file_manager.rb
@@ -1,6 +1,22 @@
 module Cumulus
   module CSV
-    BUCKET_NAME = "cumuluscsvtmp"
+    BUCKET_NAME = "cumuluscsvtmp"     
+    # DataFileManager is the gatekeeper for sending your data files to S3, and for iterating over them later. 
+    #
+    # In the constructor, It takes the same authentication parameters as aws-s3:
+    # 
+    #   DataFileManager.new(:access_key_id => 'abc',:secret_access_key => '123')  
+    #
+    # For storing your csv data file on S3, you need to setup a controller to send your uploaded files through this interface:
+    #
+    #   DataFileManager.new(connection_params).store_uploaded_file!(params[:uploaded_file])
+    #
+    # The file will be posted to S3 in a bucket set aside for this gem (it will be created upon connection if it doesn't exist already)
+    # 
+    # When you're ready to iterate over this csv file later in a background job (or wherever), you'll use this:
+    #  
+    #    DataFileManager.new(connection_params).each_row_of(name) {|row| #...whatever processing you need }
+    #   
     class DataFileManager        
       attr_reader :bucket
       
